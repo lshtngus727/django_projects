c465b4a0d3ccf66f1ce7d27f5ec0696708900cdd
lshtngus727 <lshtngus727@naver.com>
2024-09-03 18:01:57 +0900

    django project

---
+++ pybo/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    #max_length 최대 글자 200자, 길이 제한된 텍스트 CharField, 제한 없는 텍스트 TextField 사용
+    create_date = models.DateTimeField()
+    #작성일시 날짜 시간 관계된 속성 DateTimeField
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    #ForeignKey 연결키, on_delete=modles.CASACADE 답변과 연결된 질문 삭제될 경우 답변도 함께 삭제
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
+
