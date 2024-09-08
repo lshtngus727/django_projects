b6ca3f39d13e92cbd4b70e0cf6b0a2be545a77f9
lshtngus727 <lshtngus727@naver.com>
2024-09-08 22:07:18 +0900

    3-2

--- pybo/views.py
+++ pybo/views.py
@@ -1,9 +1,9 @@
 from lib2to3.fixes.fix_input import context
-
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -18,8 +18,21 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST" :
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+        else:
+            return HttpResponseNotAllowed('Only POST is possible')
+        context = {'question':question, 'form':form}
+        return render(request, 'pybo/question_detail.html',context)
     question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
 
 def question_create(request):
     if request.method == 'POST':
