aab6cda08829a119cae731063c09dc0fa91b2d75
lshtngus727 <lshtngus727@naver.com>
2024-09-15 23:03:14 +0900

    3-8 초반

--- pybo/models.py
+++ pybo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     #max_length 최대 글자 200자, 길이 제한된 텍스트 CharField, 제한 없는 텍스트 TextField 사용
@@ -11,6 +13,7 @@ class Question(models.Model):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     #ForeignKey 연결키, on_delete=modles.CASACADE 답변과 연결된 질문 삭제될 경우 답변도 함께 삭제
     content = models.TextField()
--- pybo/views.py
+++ pybo/views.py
@@ -1,11 +1,10 @@
 from lib2to3.fixes.fix_input import context
-from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     page = request.GET.get('page','1')
@@ -20,24 +19,26 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST" :
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
         else:
-            return HttpResponseNotAllowed('Only POST is possible')
+            form = AnswerForm()
         context = {'question':question, 'form':form}
         return render(request, 'pybo/question_detail.html',context)
     question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
 
-
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
